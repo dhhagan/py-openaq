b54ba138eb55736371967047dad57b693ed8a2a2
David H Hagan <david@davidhhagan.com>
2015-12-26 17:01:09 -0800

    add all the things

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 [![Build Status](https://travis-ci.org/dhhagan/py-openaq.svg?branch=master)](https://travis-ci.org/dhhagan/py-openaq)
-
 [![Coverage Status](https://coveralls.io/repos/dhhagan/py-openaq/badge.svg?branch=master&service=github)](https://coveralls.io/github/dhhagan/py-openaq?branch=master)
 
 # py-openaq
--- openaq/__init__.py
+++ openaq/__init__.py
@@ -52,79 +52,119 @@ class API(object):
         return self._send(url, 'GET', **kwargs)
 
 class OpenAQ(API):
-    ''' Instance of the OpenAQ API as documented here: https://docs.openaq.org/ '''
+    """Create an instance of the OpenAQ API
+
+    :param version: API version.
+    :param kwargs: API options.
+    """
     def __init__(self, version = 'v1', **kwargs):
         self._baseurl = 'https://api.openaq.org'
 
         super(OpenAQ, self).__init__(version = version, baseurl = self._baseurl)
 
     def cities(self, **kwargs):
-        '''
-            Provides a listing of cities within the platform.
-            kwargs include:
-                country    string   ex. country='US'
-        '''
+        """Returns a listing of cities within the platform.
+
+        :param country: Limit results by a certain country. Expects
+                        a 2-digit ISO code.
+
+        :type country: string
+        """
         return self._get('cities', **kwargs)
 
     def countries(self, **kwargs):
-        '''
-            Provides a listing of all countries within the platform
-        '''
+        """Provides a listing of all countries within the platform
+        """
         return self._get('countries', **kwargs)
 
     def latest(self, **kwargs):
-        '''
-            Provides the latest parameter for every available location in the system
-            kwargs include:
-                city       string   ex. city='Delhi'
-                country    string   ex. country='IN'
-                location   string   ex. location='Punjabi Bagh'
-                parameter  string   ex. parameter='pm25'
-                has_geo    boolean  ex. has_geo=True
-                value_from number   ex. value_from=100
-                value_to   number   ex. value_to=200
-        '''
+        """Provides the latest value of each parameter for each location
+
+        :param city: Limit results by a certain city. Defaults to ``None``.
+        :param country: Limit results by a certain country. Should be a 2-digit
+                        ISO country code. Defaults to ``None``.
+        :param location: Limit results by a city. Defaults to ``None``.
+        :param parameter: Limit results by a specific parameter. Options include [
+                            pm25, pm10, so2, co, no2, o3, bc]
+        :param has_geo: Filter items that do or do not have geographic information.
+        :param value_from: Show results above a value threshold.
+        :param value_to: Show results below a value threshold.
+
+        :type city: string
+        :type country: string
+        :type location: string
+        :type parameter: string
+        :type has_geo: boolean
+        :type value_from: number
+        :type value_to: number
+        """
         return self._get('latest', **kwargs)
 
     def locations(self, **kwargs):
-        '''
-            Provides metadata for distinct measurement locations
-            kwargs inlcude:
-                city       string   ex. city='Delhi'
-                country    string   ex. country='IN'
-                location   string   ex. location='Punjabi Bagh'
-                parameter  string   ex. parameter='pm25'
-                has_geo    boolean  ex. has_geo=True
-                value_from number   ex. value_from=100
-                value_to   number   ex. value_to=200
-                date_from  string   ex. date_from='2015-01-01' (Y-M-D)
-                date_to    string   ex. date_to='2015-12-01'   (Y-M-D)
-        '''
+        """Provides metadata about distinct measurement locations
+
+        :param city: Limit results by a certain city. Defaults to ``None``.
+        :param country: Limit results by a certain country. Should be a 2-digit
+                        ISO country code. Defaults to ``None``.
+        :param location: Limit results by a city. Defaults to ``None``.
+        :param parameter: Limit results by a specific parameter. Options include [
+                            pm25, pm10, so2, co, no2, o3, bc]
+        :param has_geo: Filter items that do or do not have geographic information.
+        :param value_from: Show results above a value threshold.
+        :param value_to: Show results below a value threshold.
+        :param date_from: Show results after a certain date. Format should be ``Y-M-D``
+        :param date_to: Show results before a certain date. Format should be ``Y-M-D``
+
+        :type city: string
+        :type country: string
+        :type location: string
+        :type parameter: string
+        :type has_geo: boolean
+        :type value_from: number
+        :type value_to: number
+        :type date_from: date
+        :type date_to: date
+        """
         return self._get('locations', **kwargs)
 
     def measurements(self, **kwargs):
-        '''
-            Provides data about individual measurements
-            kwargs include:
-                city            string   ex. city='Delhi'
-                country         string   ex. country='IN'
-                location        string   ex. location='Punjabi Bagh'
-                parameter       string   ex. parameter='pm25'
-                has_geo         boolean  ex. has_geo=True
-                value_from      number   ex. value_from=100
-                value_to        number   ex. value_to=200
-                date_from       string   ex. date_from='2015-01-01' (Y-M-D)
-                date_to         string   ex. date_to='2015-12-01'   (Y-M-D)
-                sort            string   ex. sort='asc'
-                order_by        string   ex. order_by='city'
-                include_fields  array    ex. include_fields=[location,parameter,date,value]
-                limit           number   ex. limit=500
-                page            number   ex. page=2
-                skip            number   ex. skip=500
+        """Provides metadata about distinct measurement locations
 
-            I'm not currently supporting `format` until I figure out how to deal with csv
-        '''
+        :param city: Limit results by a certain city. Defaults to ``None``.
+        :param country: Limit results by a certain country. Should be a 2-digit
+                        ISO country code. Defaults to ``None``.
+        :param location: Limit results by a city. Defaults to ``None``.
+        :param parameter: Limit results by a specific parameter. Options include [
+                            pm25, pm10, so2, co, no2, o3, bc]
+        :param has_geo: Filter items that do or do not have geographic information.
+        :param value_from: Show results above a value threshold.
+        :param value_to: Show results below a value threshold.
+        :param date_from: Show results after a certain date. Format should be ``Y-M-D``
+        :param date_to: Show results before a certain date. Format should be ``Y-M-D``
+        :param sort: The sort order (``asc`` or ``desc``).
+        :param order_by: Field to sort by. Must be used with **sort**.
+        :param include_fields: Include additional fields in the output.
+        :param limit: Change the number of results returned.
+        :param page: Paginate through the results
+        :param skip: Number of records to skip.
+
+        :type city: string
+        :type country: string
+        :type location: string
+        :type parameter: string
+        :type has_geo: boolean
+        :type value_from: number
+        :type value_to: number
+        :type date_from: date
+        :type date_to: date
+        :type sort: string
+        :type order_by: string
+        :type include_fields: array
+        :type limit: number
+        :type page: number
+        :type skip: number
+        """
         return self._get('measurements', **kwargs)
 
     def __repr__(self):
-        return "OpenAQ APi"
+        return "OpenAQ API"
